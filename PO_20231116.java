42b7b27ed1d9aa44af878a141d3e8fe7b1f4ee56
Jakub Krupicki <115708854+JAQBKRU@users.noreply.github.com>
2023-11-22 23:25:27 +0100

    Update PO_20231116.java

--- PO_20231116.java
+++ PO_20231116.java
@@ -21,7 +21,9 @@ public class Main {
 }
 
 //Zad 1
-//Napisz metodę statyczną capitalizeEverySecond, która przyjmuje jako argument obiekt typu StringBuilder. Metoda ma zmienić każdą drugą literę na wielką. Stwórz przypadek testowy.
+//Napisz metodę statyczną capitalizeEverySecond, która przyjmuje jako 
+//argument obiekt typu StringBuilder. Metoda ma zmienić każdą drugą 
+//literę na wielką. Stwórz przypadek testowy.
 class zad1{
     public static String capitalizeEverySecond(StringBuilder sb){
         for(int i = 0; i < sb.length(); i+=2){
@@ -32,7 +34,10 @@ class zad1{
 }
 
 //Zad 2
-//Napisz klasę Student, która ma pola: firstName, lastName i fieldOfStudy. Zdefiniuj w niej dwa konstruktory, jeden przyjmujący wszystkie trzy parametry, a drugi tylko firstName i lastName. Drugi konstruktor powinien wywoływać pierwszy, przekazując mu domyślną wartość fieldOfStudy jako unknown.
+//Napisz klasę Student, która ma pola: firstName, lastName i fieldOfStudy. 
+//Zdefiniuj w niej dwa konstruktory, jeden przyjmujący wszystkie trzy parametry, 
+//a drugi tylko firstName i lastName. Drugi konstruktor powinien wywoływać pierwszy, 
+//przekazując mu domyślną wartość fieldOfStudy jako unknown.
 class Student{
     public String firstName;
     public String lastName;
@@ -50,7 +55,10 @@ class Student{
 }
 
 //Zad 3
-//Zdefiniuj klasę Car, która ma pola: brand, model i productionYear. Klasa powinna zawierać dwa konstruktory: pierwszy przyjmujący wszystkie trzy parametry, a drugi tylko markę i model. Drugi konstruktor powinien wywoływać pierwszy, przekazując mu domyślną wartość productionYear jako aktualny rok.
+//Zdefiniuj klasę Car, która ma pola: brand, model i productionYear. Klasa powinna zawierać 
+//dwa konstruktory: pierwszy przyjmujący wszystkie trzy parametry, a drugi tylko markę i model. 
+//Drugi konstruktor powinien wywoływać pierwszy, przekazując mu domyślną wartość productionYear 
+//jako aktualny rok.
 class Car{
     public String brand;
     public String model;
@@ -68,7 +76,11 @@ class Car{
 }
 
 //Zad 4
-//Stwórz klasę bazową o nazwie Vehicle z polami: brand i model. Klasa ta powinna posiadać konstruktor przyjmujący oba te parametry. Następnie stwórz klasę potomną o nazwie Car, która dziedziczy po klasie Vehicle. Klasa Car powinna posiadać dodatkowe pole numberOfDoors. Stwórz konstruktor dla klasy Car, który przyjmuje wszystkie trzy parametry i korzysta z konstruktora klasy bazowej. Stwórz przypadek testowy.
+//Stwórz klasę bazową o nazwie Vehicle z polami: brand i model. Klasa ta powinna posiadać 
+//konstruktor przyjmujący oba te parametry. Następnie stwórz klasę potomną o nazwie Car, 
+//która dziedziczy po klasie Vehicle. Klasa Car powinna posiadać dodatkowe pole numberOfDoors. 
+//Stwórz konstruktor dla klasy Car, który przyjmuje wszystkie trzy parametry i korzysta z konstruktora 
+//klasy bazowej. Stwórz przypadek testowy.
 class Vehicle{
     public String brand;
     public String model;
@@ -128,8 +140,12 @@ class Book{
 
 //Zad 6
 //Wykonaj kolejno poniższe czynności:
-//1.Stwórz klasę bazową Vehicle z metodą drive, która wypisuje “The vehicle is moving.”. Następnie stwórz klasę potomną Car, która nadopisuje metodę drive tak, by wypisywała “The car is moving.”. Utwórz obiekt klasy Car i wywołaj jego metodę drive, aby zaobserwować wynik.
-//2.Na bazie klasy Vehicle z poprzedniego podpunktu, w klasie Car, nadpisz metodę drive tak, by wywoływała oryginalną metodę z klasy bazowej i dodatkowo wypisywała informacje specyficzne dla klasy Car. Użyj słowa kluczowego super, aby wywołać metodę z klasy bazowej.
+//1.Stwórz klasę bazową Vehicle z metodą drive, która wypisuje “The vehicle is moving.”. 
+//Następnie stwórz klasę potomną Car, która nadopisuje metodę drive tak, by wypisywała “The car 
+//is moving.”. Utwórz obiekt klasy Car i wywołaj jego metodę drive, aby zaobserwować wynik.
+//2.Na bazie klasy Vehicle z poprzedniego podpunktu, w klasie Car, nadpisz metodę drive tak, 
+//by wywoływała oryginalną metodę z klasy bazowej i dodatkowo wypisywała informacje specyficzne 
+//dla klasy Car. Użyj słowa kluczowego super, aby wywołać metodę z klasy bazowej.
 class Vehicle2{
     public void drive(){
         System.out.println("The vehicle is moving.");
@@ -144,7 +160,10 @@ class Car3 extends Vehicle2{
 }
 
 //Zad 7
-//Napisz statyczną metodę mergeArrays, która przyjmuje dwie tablice liczb całkowitych jako argumenty. Funkcja powinna zwrócić nową listę tablicową, która jest połączeniem obu pierwotnych tablic. Na przykład, dla list tablicowych [1, 2, 3] i [4, 5, 6], funkcja powinna zwrócić [1, 2, 3, 4, 5, 6]. Stwórz przypadek testowy.
+//Napisz statyczną metodę mergeArrays, która przyjmuje dwie tablice liczb całkowitych jako argumenty.
+// Funkcja powinna zwrócić nową listę tablicową, która jest połączeniem obu pierwotnych tablic. 
+//Na przykład, dla list tablicowych [1, 2, 3] i [4, 5, 6], funkcja powinna zwrócić [1, 2, 3, 4, 5, 6]. 
+//Stwórz przypadek testowy.
 class zad7{
     public static ArrayList<Integer> mergeArrays(int[] arr1, int[] arr2){
         ArrayList<Integer> arr = new ArrayList<>();
